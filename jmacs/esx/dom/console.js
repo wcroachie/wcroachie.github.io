56b0443f7756092dba7faf39cb55925cb05d549f
wcroachie <109880884+wcroachie@users.noreply.github.com>
2024-04-19 13:44:25 -0400

    Add files via upload

---
+++ jmacs/esx/dom/console.js
@@ -0,0 +1,98 @@
+!function(){
+
+  if( typeof esx === "undefined" ){
+    esx = {};
+  }
+
+
+  var wrapper = document.documentElement.appendChild( document.createElement("div") );
+  var wrapperStyle = 
+      "position:fixed;"
+    + "display:block;"
+    + "left:0;"
+    + "top:0;"
+    + "width:300px;"
+    + "height:150px;"
+    + "box-sizing:border-box;"
+    + "border:1px solid blue;"
+  ;
+
+  wrapper.style = wrapperStyle;
+  wrapper.setAttribute("style", wrapperStyle ); /* ie fix */
+
+
+  
+
+
+  var iframe = wrapper.appendChild( document.createElement("iframe") );
+  var iframeStyle = 
+      "position:absolute;"
+    + "display:block;"
+    + "left:2px;"
+    + "top:20px;"
+    + "width:calc(100% - 4px);"
+    + "height:calc(100% - 30px);"
+    + "box-sizing:border-box;"
+    + "border:1px solid red;"
+  ;
+  iframe.style = iframeStyle;
+  iframe.setAttribute( "style", iframeStyle ); /* ie fix */
+
+  iframe.src = "esx/dom/console.html";
+  var iframeLoaded = false;
+  
+  iframe.contentWindow.addEventListener("load",function(){
+    iframeLoaded = true;
+  });
+
+  setTimeout(function(){
+    iframeLoaded = true;
+  },1000);
+  
+
+
+
+  /* hook console methods */
+  for( var key in console ){
+    !function( key ){
+      var value = console[key];
+      if( typeof value === "function" ){
+        console[key] = function(){
+          addEntry( arguments, key );
+          return value.apply( this, arguments );
+        };
+        console["_"+key] = value;
+      }
+    }( key );
+  }
+
+  function addEntry( args, className ){
+    var div = document.createElement("div");
+    div.classList.add( className );
+    div.textContent = ">" + esx.join( args, "\n" );
+    if( iframeLoaded ){
+      iframe.contentWindow.postMessage( div.outerHTML, "*" );
+    }else{
+      console._log("waiting...");
+      setTimeout(function(){
+        addEntry( args, className );
+      })
+    }
+  }
+
+
+  setTimeout( function(){
+
+    /* update the wrapper's position at 60 fps */
+    esx.makeDraggable( wrapper, Math.floor(1000/60) );
+
+
+  }, 200 );
+
+  // var test = document.body.appendChild( document.createElement("div"));
+  // test.style = "position:fixed;left:0;top:0;width:100px;height:100px;border:1px solid gray;z-index:999999999;"
+  // esx.makeDraggable( test );
+  // test.textContent = "akjsahflkjds"
+
+
+}()
---
+++ jmacs/esx/dom/drag.js
@@ -0,0 +1,100 @@
+!function(){
+
+  if( typeof esx === "undefined" ){
+    esx = {};
+  }
+
+
+  /**
+   * use data-dragging selector to override iframe
+   * pointer events for iframes, objects, or embeds
+   * that are descendants of the dragged elem
+   **/
+  var style = document.documentElement.appendChild( document.createElement("style") );
+  style.textContent = 
+      "[data-dragging] iframe,"
+    + "[data-dragging] object,"
+    + "[data-dragging] embed{"
+    +   "pointer-events:none;"
+    +   "opacity:0.5;"
+    + "}"
+  ;
+
+  /**
+   * to minimize unnecessary dom mutations,
+   * instead of updating the position every time
+   * the pointermove event fires, this instead only
+   * updates the position every n ms, while the
+   * dragging action is taking place. in the case
+   * that for whatever reason the environment fires
+   * events more frequently than the desired visual
+   * update. an absent, falsy or otherwise invalid
+   * second argument is silently coerced to zero.
+   */
+
+  esx.makeDraggable = function( elem, updateIntervalInMs ){
+
+    var _this = this;
+
+    updateIntervalInMs = updateIntervalInMs * 1;
+    if( isNaN(updateIntervalInMs) ){
+      updateIntervalInMs = 0;
+    }
+
+    var offsetX;
+    var offsetY;
+    var iv;
+    var x, y;
+
+    function updatePosition(){
+      
+      var elemRect = _this.getClientRect( elem );
+      var parentRect = _this.getClientRect( elem.parentElement );
+
+      var minLeft = 0 - elemRect.width + 20;
+      var maxLeft = parentRect.width - 20;
+      
+      var minTop = 0 - elemRect.height + 20;
+      var maxTop = parentRect.height - 20;
+
+      x = Math.max( minLeft, x );
+      y = Math.max( minTop, y );
+      x = Math.min( maxLeft, x );
+      y = Math.min( maxTop, y );
+
+      elem.style.left = x + "px";
+      elem.style.top = y + "px";
+
+    }
+
+    function pointerDownHandler(e){
+      e.preventDefault();
+      elem.setAttribute("data-dragging","");
+      offsetX = e.clientX - _this.getClientRect(elem).x;
+      offsetY = e.clientY - _this.getClientRect(elem).y;
+      elem.removeEventListener("pointerdown",pointerDownHandler);
+      addEventListener("pointermove",pointerMoveHandler);
+      addEventListener("pointerup",pointerUpHandler);
+      iv = setInterval( updatePosition, updateIntervalInMs );
+    }
+
+    function pointerMoveHandler(e){
+      e.preventDefault();
+      x = e.clientX - offsetX;
+      y = e.clientY - offsetY;
+    }
+
+    function pointerUpHandler(e){
+      e.preventDefault();
+      elem.removeAttribute("data-dragging");
+      removeEventListener("pointerup",pointerUpHandler);
+      elem.addEventListener("pointerdown",pointerDownHandler);
+      removeEventListener("pointermove",pointerMoveHandler);
+      clearInterval( iv );
+    }
+
+    elem.addEventListener("pointerdown",pointerDownHandler);
+    
+  };
+
+}()
